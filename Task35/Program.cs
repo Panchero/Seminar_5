ee1a665f02e67dbe43db25286ed9ba038a1c8a48
Семен <marbledrop@yandex.ru>
2023-04-05 00:26:36 +0300

    Add Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 35: 
+// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1 [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+Console.Write ("Введите размер массива: ");
+
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arraySize];
+
+FillArray(array);
+WriteArray(array);
+int arraySearch = ArraySearch(array);
+Console.WriteLine($"Количество двухзначных чисел в массиве: {arraySearch}"); 
+
+void FillArray(int[]arr)
+{
+    Random rand = new Random(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0,1000);
+    }
+}
+
+void WriteArray(int[]arr)
+{
+Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write ($"{arr[i]}, "); 
+    else Console.Write ($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int ArraySearch(int[]arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)count++;
+    }
+    return count;
+}
